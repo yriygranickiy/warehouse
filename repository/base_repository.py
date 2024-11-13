62c30b1d054249748c4a8c18891fcc1766cd58f7
yriygranickiy <granickiyrepo@gmail.com>
2024-11-13 16:22:26 +0200

    1) add all service and repo in one file

--- repository/base_repository.py
+++ repository/base_repository.py
@@ -1,11 +1,28 @@
-from typing import Type, Optional, List
+from abc import abstractmethod
+from typing import Type, Optional, List, TypeVar, Protocol, Generic
 
 from sqlalchemy.orm import Session
 
-from repository.interface.interface_repository import IRepository, T
+from core.models import Category, Employee, Order, Product, Suppliers, Warehouse, WarehouseTransaction
+
+T = TypeVar('T')
+
+class Repository(Protocol, Generic[T]):
+
+    @abstractmethod
+    def get_by_id(self, id: int) -> Optional[T]:
+        ...
+
+    @abstractmethod
+    def create(self, obj: T) -> T:
+        ...
+
+    @abstractmethod
+    def get_all(self) -> List[T]:
+        ...
 
 
-class BaseRepository(IRepository[T]):
+class BaseRepository(Repository[T]):
     def __init__(self, db: Session, model: Type[T]):
         self.db = db
         self.model = model
@@ -20,3 +37,31 @@ class BaseRepository(IRepository[T]):
 
     def get_all(self) -> List[T]:
         return self.db.query(self.model).all()
+
+class CategoryRepository(BaseRepository[Category]):
+    def __init__(self, session: Session):
+        super().__init__(session,Category)
+
+class EmployeeRepository(BaseRepository[Employee]):
+    def __init__(self, session: Session):
+        super().__init__(session,Employee)
+
+class OrderRepository(BaseRepository[Order]):
+    def __init__(self, session: Session):
+        super().__init__(session,Order)
+
+class ProductRepository(BaseRepository[Product]):
+    def __init__(self, session: Session):
+        super().__init__(session,Product)
+
+class SuppliersRepository(BaseRepository[Suppliers]):
+    def __init__(self, session: Session):
+        super().__init__(session,Suppliers)
+
+class WarehouseRepository(BaseRepository[Warehouse]):
+    def __init__(self, session: Session):
+        super().__init__(session,Warehouse)
+
+class WarehouseTransactionRepository(BaseRepository[WarehouseTransaction]):
+    def __init__(self, session: Session):
+        super().__init__(session,WarehouseTransaction)
--- service/base_service.py
+++ service/base_service.py
@@ -1,11 +1,30 @@
-from typing import Generic, List
+from abc import abstractmethod
+from typing import Generic, List, TypeVar, Protocol
 
-from repository.interface.interface_repository import IRepository
-from service.interface.interface_service import IService, T
+from core.models import Category, Employee, Order, Product, Suppliers, Warehouse, WarehouseTransaction
+from repository.base_repository import Repository
+from repository.category_repository import CategoryRepository
+from repository.employee_repository import EmployeeRepository
+from repository.order_repository import OrderRepository
+from repository.product_repository import ProductRepository
+from repository.suppliers_repository import SuppliersRepository
+from repository.wearhouse_repository import WarehouseRepository
 
+T = TypeVar('T')
+class Service(Protocol,Generic[T]):
 
-class BaseService(IService[T], Generic[T]):
-    def __init__(self, repository: IRepository[T]):
+    @abstractmethod
+    def get_by_id(self, id: int) -> T:
+        ...
+    @abstractmethod
+    def create(self, obj: T) -> T:
+        ...
+    @abstractmethod
+    def get_all(self)->List[T]:
+        ...
+
+class BaseService(Service[T], Generic[T]):
+    def __init__(self, repository: Repository[T]):
         self.repository = repository
 
     def get_by_id(self, id: int) -> T:
@@ -16,3 +35,32 @@ class BaseService(IService[T], Generic[T]):
 
     def get_all(self) -> List[T]:
         return self.repository.get_all()
+
+
+class CategoryService(BaseService[Category]):
+    def __init__(self, repository: CategoryRepository):
+        super().__init__(repository)
+
+class EmployeeService(BaseService[Employee]):
+    def __init__(self, repository: EmployeeRepository):
+        super().__init__(repository)
+
+class OrderService(BaseService[Order]):
+    def __init__(self, repository: OrderRepository):
+        super().__init__(repository)
+
+class ProductService(BaseService[Product]):
+    def __init__(self, repository: ProductRepository):
+        super().__init__(repository)
+
+class SupplierService(BaseService[Suppliers]):
+    def __init__(self, repository: SuppliersRepository):
+        super().__init__(repository)
+
+class WarehouseService(BaseService[Warehouse]):
+    def __init__(self, repository: WarehouseRepository):
+        super().__init__(repository)
+
+class WarehouseTransactionRepository(BaseService[WarehouseTransaction]):
+    def __init__(self, repository: WarehouseRepository):
+        super().__init__(repository)
