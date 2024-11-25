ed0b7dec81b812f5c4d67727364375179bf19fb1
yriygranickiy <granickiyrepo@gmail.com>
2024-11-25 15:34:03 +0200

    1) add view for employee

--- core/base_view.py
+++ core/base_view.py
@@ -1,30 +1,91 @@
+import time
 from abc import abstractmethod
-from datetime import time
-from typing import TypeVar, Generic, Protocol
+from typing import TypeVar, Generic, Protocol, List
 
-from core.base_repository import Repository
-from core.base_service import Service
+from core.base_repository import EmployeeRepository, Repository
+from core.base_service import Service, EmployeeService
+from core.models import Employee
+from database import SessionLocal
 
 T = TypeVar('T')
 
-class View(Protocol, Generic[T]):
+session = SessionLocal()
 
+
+class View(Protocol):
     @abstractmethod
     def get_view_all(self):
         ...
 
 
-class BaseView(View[T],Generic[T]):
+class BaseView(View):
 
-    def __init__(self,service:Service[T]):
+    def __init__(self, service):
         self.service = service
 
     def get_view_all(self):
-        print("Load list transaction...")
-        for obj in self.service.get_all():
-            print(obj)
-            time.sleep(0.3)
-        print("list load successfully!")
+        self.get_view_all()
 
 
+class EmployeeView(BaseView):
+    def __init__(self, service: EmployeeService):
+        super().__init__(service)
+    #
+    # repository = EmployeeRepository(session)
+    # service = EmployeeService(repository)
 
+    def get_view_all(self):
+        employee_loop_menu = True
+
+        while employee_loop_menu:
+            print("\nEmployee:\n")
+            print("1. Add")
+            print("2. Delete")
+            print("3. Update")
+            print("4. Get all")
+            print("5. Get employee by id")
+            print("6. Exit")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                first_name = input("Enter your first name: ")
+                last_name = input("Enter your last name: ")
+                email = input("Enter your email: ")
+                phone = input("Enter your phone number: ")
+                created_employee = Employee(first_name=first_name,
+                                            last_name=last_name,
+                                            position="staff",
+                                            email=email,
+                                            phone=phone)
+                print("Employee adding....")
+                self.create(created_employee)
+                time.sleep(0.3)
+                print("Employee add successfully!")
+            elif choice == "2":
+                print("Input employee id for deleting")
+                employee_id = int(input("Enter employee id: "))
+                print("Deleting employee...")
+                time.sleep(0.3)
+                self.delete(employee_id)
+                print("Employee delete successfully!")
+            elif choice == "3":
+                pass
+            elif choice == "4":
+                print("load list employee...")
+                time.sleep(0.3)
+                for employee in self.get_all():
+                    print(employee)
+                    time.sleep(0.3)
+                print("list load successfully!")
+            elif choice == "5":
+                employee_id = input("Enter employee id: ")
+                employee = self.get_by_id(employee_id)
+                print("getting employee data...")
+                time.sleep(0.3)
+                print(employee)
+                print("employee data get successfully!")
+            elif choice == "6":
+                employee_loop_menu = False
+            else:
+                print("Invalid choice")
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from core.base_repository import EmployeeRepository, CategoryRepository, OrderRe
 from core.base_service import EmployeeService, CategoryService, OrderService, ProductService, SupplierService, \
     WarehouseService, WarehouseTransactionService
 from database import init_db, SessionLocal
+from core.base_view import EmployeeView
 
 init_db()
 session = SessionLocal()
@@ -29,8 +30,12 @@ def main():
         print("8. Exit")
 
         choice = input("Enter your choice: ")
+
         if choice == "1":
-            employee_view()
+            repository = EmployeeRepository(session)
+            service = EmployeeService(repository)
+            EmployeeView.get_view_all(service)
+            # employee_view()
         elif choice == "2":
             category_view()
         elif choice == "3":
@@ -49,62 +54,62 @@ def main():
             print("Invalid choice")
 
 
-def employee_view():
-    employee_repository = EmployeeRepository(session)
-    employee_service = EmployeeService(repository=employee_repository)
-
-    employee_loop_menu = True
-    while employee_loop_menu:
-        print("\nEmployee:\n")
-        print("1. Add")
-        print("2. Delete")
-        print("3. Update")
-        print("4. Get all")
-        print("5. Get employee by id")
-        print("6. Exit")
-
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            first_name = input("Enter your first name: ")
-            last_name = input("Enter your last name: ")
-            email = input("Enter your email: ")
-            phone = input("Enter your phone number: ")
-            created_employee = Employee(first_name=first_name,
-                                        last_name=last_name,
-                                        position="staff",
-                                        email=email,
-                                        phone=phone)
-            print("Employee adding....")
-            employee_service.create(created_employee)
-            time.sleep(0.3)
-            print("Employee add successfully!")
-        elif choice == "2":
-            print("Input employee id for deleting")
-            employee_id = int(input("Enter employee id: "))
-            print("Deleting employee...")
-            time.sleep(0.3)
-            employee_service.delete(employee_id)
-            print("Employee delete successfully!")
-        elif choice == "3":
-            pass
-        elif choice == "4":
-            print("load list employee...")
-            time.sleep(0.3)
-            for employee in employee_service.get_all():
-                print(employee)
-                time.sleep(0.3)
-            print("list load successfully!")
-        elif choice == "5":
-            employee_id = input("Enter employee id: ")
-            employee = employee_service.get_by_id(employee_id)
-            print("getting employee data...")
-            time.sleep(0.3)
-            print(employee)
-            print("employee data get successfully!")
-        elif choice == "6":
-            employee_loop_menu = False
-        else:
-            print("Invalid choice")
+# def employee_view():
+#     employee_repository = EmployeeRepository(session)
+#     employee_service = EmployeeService(repository=employee_repository)
+#
+#     employee_loop_menu = True
+#     while employee_loop_menu:
+#         print("\nEmployee:\n")
+#         print("1. Add")
+#         print("2. Delete")
+#         print("3. Update")
+#         print("4. Get all")
+#         print("5. Get employee by id")
+#         print("6. Exit")
+#
+#         choice = input("Enter your choice: ")
+#         if choice == "1":
+#             first_name = input("Enter your first name: ")
+#             last_name = input("Enter your last name: ")
+#             email = input("Enter your email: ")
+#             phone = input("Enter your phone number: ")
+#             created_employee = Employee(first_name=first_name,
+#                                         last_name=last_name,
+#                                         position="staff",
+#                                         email=email,
+#                                         phone=phone)
+#             print("Employee adding....")
+#             employee_service.create(created_employee)
+#             time.sleep(0.3)
+#             print("Employee add successfully!")
+#         elif choice == "2":
+#             print("Input employee id for deleting")
+#             employee_id = int(input("Enter employee id: "))
+#             print("Deleting employee...")
+#             time.sleep(0.3)
+#             employee_service.delete(employee_id)
+#             print("Employee delete successfully!")
+#         elif choice == "3":
+#             pass
+#         elif choice == "4":
+#             print("load list employee...")
+#             time.sleep(0.3)
+#             for employee in employee_service.get_all():
+#                 print(employee)
+#                 time.sleep(0.3)
+#             print("list load successfully!")
+#         elif choice == "5":
+#             employee_id = input("Enter employee id: ")
+#             employee = employee_service.get_by_id(employee_id)
+#             print("getting employee data...")
+#             time.sleep(0.3)
+#             print(employee)
+#             print("employee data get successfully!")
+#         elif choice == "6":
+#             employee_loop_menu = False
+#         else:
+#             print("Invalid choice")
 
 
 def category_view():
